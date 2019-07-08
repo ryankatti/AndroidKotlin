1aeefd086f1173dc2a50aa613a261a2fbd9ad52c
Ramesh Yankatti <ryankatti@netpace.com>
2019-07-07 23:23:47 -0700

    Intial draft of kotlin android

---
+++ app/src/main/java/com/liri/listview/ListViewActivity.kt
@@ -0,0 +1,13 @@
+package com.liri.listview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rgy.androidkotlin.R
+
+class ListViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_view)
+    }
+}
